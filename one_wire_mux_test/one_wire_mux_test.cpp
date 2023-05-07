7a26890d62de3b35e8eb6660985a386300d20545
William Vinnicombe <william.vinnicombe@gmail.com>
2023-05-07 23:48:27 +0100

    Cleanup onewiremux test

--- one_wire_mux_test/one_wire_mux_test.cpp
+++ one_wire_mux_test/one_wire_mux_test.cpp
@@ -34,34 +34,5 @@ int main() {
         }
     }
 
-    rom_address_t address{};
-    while (true) {
-        one_wire.single_device_read_rom(address);
-        printf("Device Address: %02x%02x%02x%02x%02x%02x%02x%02x\n", address.rom[0], address.rom[1], address.rom[2], address.rom[3], address.rom[4], address.rom[5], address.rom[6], address.rom[7]);
-
-        for (int i=0; i < 8; i++)
-            one_wire.ram[i] = i;
-        one_wire.write_scratch_pad(address, 8);
-
-        sleep_ms(10);
-
-        one_wire.read_scratch_pad(address);
-        printf("TA1: %02x TA2: %02x E/S: %02x\n", one_wire.ta1, one_wire.ta2, one_wire.e_s);
-        for (int i=0; i < 10; i++)
-            printf("%02x", one_wire.ram[i]);
-        printf("\n");
-
-        int ret = one_wire.copy_scratch_pad(address);
-        printf("Ret: %d\n", ret);
-
-        uint8_t data[16] = {};
-        one_wire.read_memory(address, 0, data, 16);
-        for (int i=0; i < 16; i++)
-            printf("%02x", data[i]);
-        printf("\n");
-
-        sleep_ms(1000);
-    }
-
     return 0;
 }
